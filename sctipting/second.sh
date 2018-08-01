5a42b1a44a7053b9925d8954400a1ecd3f06860b
AviSoomirtee <avisoomirtee.as@gmail.com>
2018-08-01 22:05:29 +0400

    completed function proxy and cron

--- sctipting/second.sh
+++ sctipting/second.sh
@@ -72,28 +72,80 @@ function test_proxy () { #add proxy in agent and run inventaire/ when call add p
 
 }
 
-
-function add_cron_proxy () {
-	proXy=$1
-	echo "Adding proxy..."
-	echo "sudo -i" > $BASH
-	echo "cd /etc/cron.d/" >> $BASH
-	echo "touch inventaire_proxy" >> $BASH
-	TEMP_PROXY="$MIN $HOUR * * * root echo $PROXY"
-	TEMP2_PROXY="$TEMP_PROXY >> /usr/local/linkbynet/script/inventaire/etc/ocsinventory-agent.cfg"
-	echo "echo '$TEMP2_PROXY'  >> inventaire_proxy " >> $BASH
-	echo "exit" >> $BASH
-	echo "exit" >> $BASH
-	cat $BASH | ssh -t -t $HOST
+function cron_job () {					#takes as argument 1:proxy 2:PROXY value or 1:ssl
+	echo "Checking for cron job for inventaire "
+	read MIN <<< $(sed -n 4p $CMD | ssh -t $HOST)
+	sleep 2
+	read HOUR <<< $(sed -n 5p $CMD | ssh -t $HOST)
 	echo " "
-	echo "Cron job added for proxy."
-	
+	echo "Time acquired"
+	echo "Inventaire running at: $HOUR $MIN"
+
+	if [ $HOUR="4" ] && [ $MIN="23" ]
+	then
+		MIN1="22"
+		MIN2="46"
+		HOUR2="5"
+		MIN3="16"
+	elif [ $HOUR="0" ] && [ $MIN="6" ]
+	then
+		MIN1="5"
+		MIN2="16"
+		HOUR2="6"
+		MIN3="46"
+	else
+		echo "Cronjob is set for an odd time."
+	fi
+
+	if [ $1="proxy" ]  #for proxy
+	then
+		if [ -z "$2" ] #value of proxy
+		then
+			echo "Taking proxy and creating cronjob."
+			local PROXY=$2
+			echo "sudo -i" > $BASH
+			echo "cd /etc/cron.d/" >> $BASH
+			echo "touch inventaire_proxy" >> $BASH
+			
+			TEMP_PROXY="$MIN1 $HOUR * * * root echo $PROXY"
+			TEMP2_PROXY="$TEMP_PROXY >> /usr/local/linkbynet/script/inventaire/etc/ocsinventory-agent.cfg"
+			echo "echo '$TEMP2_PROXY'  >> inventaire_proxy " >> $BASH
+			
+			TEMP_PROXY1="$MIN2 $HOUR * * * root echo $PROXY"
+			TEMP2_PROXY1="$TEMP_PROXY1 >> /usr/local/linkbynet/script/inventaire/etc/ocsinventory-agent.cfg"
+			echo "echo '$TEMP2_PROXY1'  >> inventaire_proxy " >> $BASH
+
+			TEMP_PROXY2="$MIN3 $HOUR2 * * * root echo $PROXY"
+			TEMP2_PROXY2="$TEMP_PROXY2 >> /usr/local/linkbynet/script/inventaire/etc/ocsinventory-agent.cfg"
+			echo "echo '$TEMP2_PROXY2'  >> inventaire_proxy " >> $BASH
+
+			echo "exit" >> $BASH
+			echo "exit" >> $BASH
+			cat $BASH | ssh -t -t $HOST
+			echo " "
+			echo "Cron job added for proxy."
+		fi
+	elif [ $1="ssl" ] #for bypassing ssl
+	then
+		echo "Creating cron job for bypassing ssl"
+		echo "sudo -i" > $BASH
+		echo "cd /etc/cron.d/" >> $BASH
+		echo "touch inventaire_ssl" >> $BASH
+
+		TEMP_SSL= "$MIN1 $HOUR * * * root echo 'ssl=0' >> /usr/local/linkbynet/script/inventaire/etc/ocsinventory-agent.cfg"
+		echo "echo '$TEMP_SSL' >> inventaire_proxy " >> $BASH
+
+		TEMP_SSL= "$MIN2 $HOUR * * * root echo 'ssl=0' >> /usr/local/linkbynet/script/inventaire/etc/ocsinventory-agent.cfg"
+		echo "echo '$TEMP_SSL' >> inventaire_proxy " >> $BASH
+
+		TEMP_SSL= "$MIN3 $HOUR2 * * * root echo 'ssl=0' >> /usr/local/linkbynet/script/inventaire/etc/ocsinventory-agent.cfg"
+		echo "echo '$TEMP_SSL' >> inventaire_proxy " >> $BASH
+	fi
 }
 
 
 
-
-echo Scritp started.
+echo "Scritp started."
 
 usage () {
 
